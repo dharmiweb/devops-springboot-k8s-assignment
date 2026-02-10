91bafa2fee108a11b8507c292e783183d58c700b
bhagyesh1 <bhagyeshvaniya00@gmail.com>
2026-02-10 14:04:28 +0530

    fix: hellocontroller.java

--- app/sample-spring-boot-app/src/main/java/com/example/sample_spring_boot_app/HelloController.java
+++ app/sample-spring-boot-app/src/main/java/com/example/sample_spring_boot_app/HelloController.java
@@ -1,3 +1,8 @@
+package com.example.sample_spring_boot_app;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class HelloController {
 
